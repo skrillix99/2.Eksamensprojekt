4f6e1a81602ed93e93d98516adee1d62e528c15a
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2022-04-28 15:39:52 +0200

    tilføjet razor pages

---
+++ 2.Eksamensprojekt/Pages/Shared/LedigeLokaler.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace _2.Eksamensprojekt.Pages.Shared
+{
+    public class LedigeLokalerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

89ea1cbaaee1a6376480a70c579203e5636ad9d6
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2022-04-28 11:47:22 +0200

    BookingData tilføjet

---
+++ SuperBookerData/BookingData.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperBookerData
+{
+    class BookingData
+    {
+        private TimeSpan _tidsrum;
+        private DateTime _dag;
+        private bool _heltBooket;
+        
+        public TimeSpan Tidsrum
+        {
+            get => _tidsrum;
+            set => Tidsrum = value;
+        }
+
+        public DateTime Dag
+        {
+            get => _dag;
+            set => Dag = value;
+        }
+
+        public bool HeltBooket
+        {
+            get => _heltBooket;
+            set => HeltBooket = value;
+        }
+
+        public BookingData()
+        {
+
+        }
+
+        public BookingData(TimeSpan Tidsrum, DateTime Dag, bool HeltBooket)
+        {
+            _tidsrum = Tidsrum;
+            _dag = Dag;
+            _heltBooket = HeltBooket;
+        }
+    }
+}

642b7a72e716c856ad4fecf901a77491229e6260
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2022-04-28 10:49:15 +0200

    SuberBookerData Library tilføjet

---
+++ SuperBookerData/PersonData.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SuperBookerData
+{
+    public class PersonData
+    {
+        private int _brugerID;
+        private string _brugerNavn;
+        private string _brugerTlf;
+        private string _brugerEmail;
+        private string _brugerPassword;
+
+        public int brugerID
+        {
+            get => _brugerID;
+            set => brugerID = value;
+        }
+
+        public string brugerNavn
+        {
+            get => _brugerNavn;
+            set => brugerNavn = value;
+        }
+
+        public string brugerTlf
+        {
+            get => _brugerTlf;
+            set => brugerTlf = value;
+        }
+
+        public string brugerEmail
+        {
+            get => _brugerEmail;
+            set => brugerEmail = value;
+        }
+
+        public string brugerPassword
+        {
+            get => _brugerPassword;
+            set => brugerPassword = value;
+        }
+
+        public PersonData()
+        {
+
+        }
+
+        public PersonData(int brugerID, string brugerNavn, string brugerTlf, string brugerEmail, string brugerPassword)
+        {
+            _brugerID = brugerID;
+            _brugerNavn = brugerNavn;
+            _brugerTlf = brugerTlf;
+            _brugerEmail = brugerEmail;
+            _brugerPassword = brugerPassword;
+        }
+
+    }
+}

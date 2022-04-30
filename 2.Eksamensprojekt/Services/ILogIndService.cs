ef94fe030a081a00086fdfd92897a760ca7325b6
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2022-04-30 19:47:46 +0200

    interfaces tilføjet i Services Mappe

---
+++ 2.Eksamensprojekt/Services/ILogIndService.cs
@@ -0,0 +1,15 @@
+﻿using SuperBookerData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _2.Eksamensprojekt.Services
+{
+    interface ILogIndService
+    {
+        public bool Contains(LogIndData LogInd);
+
+        public brugerRolle ContainsAndGiveRole(LogIndData LogInd);
+    }
+}

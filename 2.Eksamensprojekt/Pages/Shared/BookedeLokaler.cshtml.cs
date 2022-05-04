2805842114a1907d322b373bcdcc0b408d618918
Dennis Møllegård Hansen <denn108g@edu.easj.dk>
2022-05-04 13:37:55 +0200

    bookede lokaler back end

--- 2.Eksamensprojekt/Pages/Shared/BookedeLokaler.cshtml.cs
+++ 2.Eksamensprojekt/Pages/Shared/BookedeLokaler.cshtml.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using _2.Eksamensprojekt.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SuperBookerData;
 
 namespace _2.Eksamensprojekt.Pages.Shared
 {
     public class BookedeLokalerModel : PageModel
     {
-        public void OnGet()
-        {
 
+        private IBookingService _bookingService;
+
+        public BookedeLokalerModel(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
         }
+
+        [BindProperty]
+        public LokaleData LokaleData { get; set; }
+
+        public void OnGet(int LokaleID)
+        {
+            LokaleData = _bookingService.GetById(LokaleID);
+        }
+
+
+        public IActionResult OnPost(int LokaleID)
+        {
+            LokaleData deletedLokale = _bookingService.Delete(LokaleID);
+
+            return RedirectToPage("Index");
+        }
+
     }
 }
 

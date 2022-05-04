86a0c19f83d6f1e97baf53209ca511d1b8900859
Dennis Møllegård Hansen <denn108g@edu.easj.dk>
2022-05-04 10:04:47 +0200

    dfg

--- 2.Eksamensprojekt/Pages/Shared/BookedeLokaler.cshtml.cs
+++ 2.Eksamensprojekt/Pages/Shared/BookedeLokaler.cshtml.cs
@@ -11,6 +11,42 @@ namespace _2.Eksamensprojekt.Pages.Shared
     {
         public void OnGet()
         {
+
         }
+
+        private const string connectionString = @"Data Source=zealandmarc.database.windows.net;Initial Catalog=SuperBooker4000;User ID=AdminMarc;Password=********;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public List<Bookings> GetAll()
+        {
+
+            String sql = "select * from Reservation";
+
+            // opret forbindelse
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                // åbner forbindelsen
+                connection.Open();
+
+                // opretter sql query
+                SqlCommand cmd = new SqlCommand(sql, connection);
+
+                // altid ved select
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                // læser alle rækker
+                while (reader.Read())
+                {
+                    Car car = ReadCar(reader);
+                    liste.Add(car);
+                }
+            }
+
+            return liste;
+        }
+
+
+
+
     }
 }
+

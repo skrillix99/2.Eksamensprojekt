e1fbb71923643b7a41ba0f1adf2ea1c7469ad694
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-05-06 09:44:28 +0200

    -f

--- 2.Eksamensprojekt/Pages/Shared/LedigeLokaler.cshtml.cs
+++ 2.Eksamensprojekt/Pages/Shared/LedigeLokaler.cshtml.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.IdentityModel.Claims;
 using SuperBookerData;
 using Claim = Microsoft.IdentityModel.Claims.Claim;
-using SuperBookerData;
 
 namespace _2.Eksamensprojekt.Pages.Shared
 {
@@ -18,10 +17,6 @@ namespace _2.Eksamensprojekt.Pages.Shared
         private ILedigeLokalerService _ledigeLokalerService;
         private static List<LokaleData> _lokaleListe;
         public List<LokaleData> LokaleData { get; private set; }
-
-        public string TestA { get; set; }
-        public string TestB { get; set; }
-        public string TestC { get; set; }
         public LedigeLokalerModel(ILedigeLokalerService ledigeLokalerService)
         {
             _ledigeLokalerService = ledigeLokalerService;
@@ -31,50 +26,9 @@ namespace _2.Eksamensprojekt.Pages.Shared
         {
             _lokaleListe = _ledigeLokalerService.GetAll();
             LokaleData = new List<LokaleData>(_lokaleListe);
-            IClaimsPrincipal icp = User as IClaimsPrincipal;
-
-            //IClaimsIdentity claimsIdentity = (IClaimsIdentity) icp.Identity;
-
-            foreach (var claim in User.Claims)
-            {
-                if (claim.Value == "jepp198l@zealand.dk")
-                {
-                    TestA = claim.Value;
-                }
-                if (claim.Value == "marx3796@zealand.dk")
-                {
-                    TestB = claim.Value;
-                }
-
-                if (claim.Value == "alex650w@zealand.dk")
-                {
-                    TestC = claim.Value;
-                }
-            }
+          
         }
 
-        public IActionResult OnPost()
-        {
-            foreach (var claim in User.Claims)
-            {
-                if (claim.Value == "jepp198l@zealand.dk")
-                {
-                    TestA = claim.Value;
-                    return RedirectToPage("/AdministrationPages/AdministrationBooking");
-                }
-                if (claim.Value == "marx3796@zealand.dk")
-                {
-                    TestB = claim.Value;
-                    return RedirectToPage("/UnderviserPages/UnderviserBooking");
-                }
-                if (claim.Value == "alex650w@zealand.dk")
-                {
-                    TestC = claim.Value;
-                    return RedirectToPage("/StuderendePages/StuderendeBooking");
-                }
-            }
-
-            return Page();
-        }
+       
     }
 }

d93aa32a8f6f8b481e5fd19e29732caeae7d658e
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-05-16 10:36:45 +0200

    kekw

--- 2.Eksamensprojekt/Services/AdministrationService.cs
+++ 2.Eksamensprojekt/Services/AdministrationService.cs
@@ -300,13 +300,8 @@ namespace _2.Eksamensprojekt.Services
         /// Sletter en booking fra databasen baseret på id
         /// </summary>
         /// <param name="id">Typen int. Indeholder id'et på den booking der skal slettes</param>
-        public void DeleteResevation(int id)
+        public void DeleteReservation()
         {
-            if (id <= 0)
-            {
-                throw new KeyNotFoundException("Der findes ikke nogle reservationer med det ID");
-            }
-
             //string sql = "DELETE from Reservation WHERE ReservationID = @id";
             string sql = "DELETE from Reservation WHERE Dag < @nextDay";
 

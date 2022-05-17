16d975ff4b8885520ccda0b8f9fc011fb204868c
Jonathan Valdemar Elberg <jona725k@edu.easj.dk>
2022-05-17 10:32:46 +0200

    fdjk

--- 2.Eksamensprojekt/Pages/Shared/BookedeLokaler.cshtml.cs
+++ 2.Eksamensprojekt/Pages/Shared/BookedeLokaler.cshtml.cs
@@ -4,12 +4,16 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using _2.Eksamensprojekt.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SuperBookerData;
 
 namespace _2.Eksamensprojekt.Pages.Shared
 {
+    [Authorize(Roles = "Underviser")]
+    [Authorize(Roles = "Administration")]
+
     public class BookedeLokalerModel : PageModel
     {
 

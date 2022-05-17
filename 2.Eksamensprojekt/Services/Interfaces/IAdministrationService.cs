4ade9d0ffc4c90d9abc52a98aa976b304f058f4e
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-05-17 14:47:20 +0200

    Added Update funktion til Administration

--- 2.Eksamensprojekt/Services/Interfaces/IAdministrationService.cs
+++ 2.Eksamensprojekt/Services/Interfaces/IAdministrationService.cs
@@ -38,6 +38,8 @@ namespace _2.Eksamensprojekt.Services.Interfaces
         /// <param name="newBooking">BookingData object. Skal have følgene TidStart, Dag, Bruger.BrugerEmail og BookesFor</param>
         void AddReservation(BookingData newBooking);
 
+        void UpdateReservation(BookingData updatedBooking);
+
 
     }
 }

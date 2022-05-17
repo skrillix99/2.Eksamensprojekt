bdd84c608725997285e8903742a627d57ee17115
Anders Zune Sander Petersen <ande939h@edu.easj.dk>
2022-05-17 14:17:17 +0200

    Login og log ud knapperne er sort/hvid på alle sider

--- 2.Eksamensprojekt/Services/UnderviserService.cs
+++ 2.Eksamensprojekt/Services/UnderviserService.cs
@@ -6,7 +6,7 @@ using SuperBookerData;
 
 namespace _2.Eksamensprojekt.Services
 {
-    public class UnderviserService: IUnderviserService
+    public class UnderviserService : IUnderviserService
     {
         private const string connectionString = "Data Source=zealandmarc.database.windows.net;Initial Catalog=SuperBooker4000;User ID=AdminMarc;Password=Marcus19;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         private readonly ILogIndService _logIndService;

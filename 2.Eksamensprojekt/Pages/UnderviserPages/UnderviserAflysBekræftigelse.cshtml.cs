34ee722dccca4c6e7ea1fabea05ca8bd0b2d0fd3
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-05-19 15:20:17 +0200

    fixed underviser delete

--- 2.Eksamensprojekt/Pages/UnderviserPages/UnderviserAflysBekræftigelse.cshtml.cs
+++ 2.Eksamensprojekt/Pages/UnderviserPages/UnderviserAflysBekræftigelse.cshtml.cs
@@ -24,7 +24,7 @@ namespace _2.Eksamensprojekt.Pages.UnderviserPages
         }
         public void OnGet()
         {
-        
+            Booking = UnderviserAflysBookingModel.TempBookingData;
         }
 
         public void OnPost(int id)

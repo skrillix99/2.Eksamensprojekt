d300b46976ebb03ea8489de09c35dfa48583327c
Marcus Vibe Hjulgaard <marc3277@edu.easj.dk>
2022-05-20 22:08:38 +0200

    dag

--- 2.Eksamensprojekt/Services/StuderendeService.cs
+++ 2.Eksamensprojekt/Services/StuderendeService.cs
@@ -5,7 +5,7 @@ using _2.Eksamensprojekt.Pages.AdministrationPages;
 using _2.Eksamensprojekt.Services.Interfaces;
 using SuperBookerData;
 
-namespace _2.Eksamensprojekt.Services
+namespace _2.Eksamensprojekt.Services // Marcus
 {
     public class StuderendeService: IStuderendeService
     {
@@ -15,7 +15,7 @@ namespace _2.Eksamensprojekt.Services
         private readonly IAdministrationService _administrationService;
         private readonly IBookingService _bookingService;
         /// <summary>
-        /// Laver dependency injection til at kunne bruge ILogIndService.
+        /// Laver dependency injection til at kunne bruge services.
         /// </summary>
         /// <param name="personService">Typen IPersonService</param>
         public StuderendeService(IPersonService personService, ILokalerService lokalerService, IAdministrationService administrationService, IBookingService bookingService)
@@ -83,8 +83,10 @@ namespace _2.Eksamensprojekt.Services
 
 
         /// <summary>
-        /// Reservere et lokale for en studerende baseret på deres BrugerID. Hvis lokalet ikke er blevet booket før
-        /// så kalder den GetSingelLokale() og tager MuligeBookinger og minusser den med 1, som den giver HeltBooket.
+        /// Reservere et lokale for en studerende baseret på deres BrugerID og BookingDataen der bliver sendt med i parameteret.
+        /// Tjekker om brugeren har for mange bookinger.
+        /// Finder ud af om der er nogle som har booket lokale på den samme dag og om lokalet er heltbooket eller ej.
+        /// Hvis lokalet ikke er blevet booket før så kalder den GetSingelLokale() og tager MuligeBookinger og minusser den med 1, som den giver HeltBooket.
         /// Hvis HeltBooket = 0 så thrower den en ArgumentOutOfRangeException.
         /// hvis kaldet til databasen fejler så thrower den en Execption
         /// </summary>
@@ -99,6 +101,7 @@ namespace _2.Eksamensprojekt.Services
             int BrugerID = _personService.GetSingelPersonByEmail(newBooking.BrugerEmail).BrugerID;
             int limit = (int) _administrationService.GetAllStuderendeRettigheder()[1];
             int antalBooket = CheckReservationerByBrugerId(BrugerID).Count;
+            //tjekker om brugeren har for mange bookinger
             if (antalBooket <= limit)
             {
                 msg = $"Du har nu {limit - CheckReservationerByBrugerId(BrugerID).Count} tilbage";
@@ -107,28 +110,34 @@ namespace _2.Eksamensprojekt.Services
             {
                 throw new ArgumentOutOfRangeException($"Du må kun have {limit} bookings ad gangen");
             }
-
+            //failsafe. hvis lokaleID er 0 så finder vi den rigtige.
             if (newBooking.Lokale.LokaleID == 0)
             {
                 newBooking.Lokale.LokaleID = _bookingService.GetSingleBooking(newBooking.ResevertionId).Lokale.LokaleID;
             }
 
+            //findbinder til databasen via connectionString i toppen af klassen
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 BookingData tilbage = GetSingleBookingByIdAndDay(newBooking.Lokale.LokaleID, newBooking.Dag);
+                //hvis der ikke er nogle bookinger i det lokale på den dag så finder den ud af
+                //hvor mange der kan booke lokalet fra starten og minusser det med 1 senere henne
                 if (tilbage == null)
                 {
                     tilbage = new BookingData();
                     tilbage.HeltBooket = _lokalerService.GetSingelLokale(newBooking.Lokale.LokaleID).MuligeBookinger;
                 }
+                // hvis lokalet er booket på den dag så tjekker vi om heltbooket er 0
                 else if (tilbage.HeltBooket == 0)
                 {
                     throw new ArgumentOutOfRangeException("Der er ikke flere bookings ledige på dette lokale");
                 }
 
+                // laver vores SQL sætninger klar til SqlCommand
                 string sql = "insert into Reservation VALUES (@tidStart, @dag, @mulige, @brugerFK, @lokaleFK, @tidSlut, 0, @smartboard)";
-                string sqlUpdate = "UPDATE Reservation SET HeltBooket = @heltBooket WHERE LokaleID_FK = @id";
+                string sqlUpdate = "UPDATE Reservation SET HeltBooket = @heltBooket WHERE LokaleID_FK = @id AND Dag = @dag";
 
+                // kører sql sætningen på databasen med de parameter vi sender med.
                 SqlCommand cmd = new SqlCommand(sql, connection);
                 cmd.Parameters.AddWithValue("@tidStart", newBooking.TidStart);
                 cmd.Parameters.AddWithValue("@dag", newBooking.Dag.Date);
@@ -138,6 +147,7 @@ namespace _2.Eksamensprojekt.Services
                 cmd.Parameters.AddWithValue("@lokaleFK", newBooking.Lokale.LokaleID);
                 cmd.Parameters.AddWithValue("@smartboard", newBooking.BooketSmartBoard);
 
+                //åbner for forbindelsen til databasen.
                 cmd.Connection.Open();
 
                 int rows = cmd.ExecuteNonQuery();
@@ -146,12 +156,14 @@ namespace _2.Eksamensprojekt.Services
                 {
                     throw new Exception("welp");
                 }
+                // lukker for forbindelsen.
                 cmd.Connection.Close();
 
-
+                // updatere Heltbooket med - 1 baseret på lokaleId og Dag
                 SqlCommand cmdUpdate = new SqlCommand(sqlUpdate, connection);
                 cmdUpdate.Parameters.AddWithValue("@heltBooket", tilbage.HeltBooket - 1);
                 cmdUpdate.Parameters.AddWithValue("@id", newBooking.Lokale.LokaleID);
+                cmdUpdate.Parameters.AddWithValue("@dag", newBooking.Dag);
 
                 cmdUpdate.Connection.Open();
 
@@ -165,6 +177,10 @@ namespace _2.Eksamensprojekt.Services
             return msg;
         }
 
+        /// <summary>
+        /// Sletter en reservation baseret på id'et fra parameteret
+        /// </summary>
+        /// <param name="id">typen Int. Skal være et gyldigt reservations id</param>
         public void DeleteReservation(int id)
         {
             string sql = "DELETE from Reservation WHERE ReservationID = @id";
@@ -202,6 +218,11 @@ namespace _2.Eksamensprojekt.Services
             }
         }
 
+        /// <summary>
+        /// Finder ReservationID på alle de bookinger en bruger har via parameter id'et
+        /// </summary>
+        /// <param name="id">Typen Int. Skal være et gyldigt brugerID</param>
+        /// <returns>Liste object af BookingData</returns>
         public List<BookingData> CheckReservationerByBrugerId(int id)
         {
             List<BookingData> lokaler = new List<BookingData>();
@@ -231,7 +252,12 @@ namespace _2.Eksamensprojekt.Services
 
             return lokaler;
         }
-
+        /// <summary>
+        /// Finder ud af om lokalet er booket på den dag via parameterne.
+        /// </summary>
+        /// <param name="id">Typen Int. Skal være et LokaleID</param>
+        /// <param name="dag">Typen DateTime. Skal være dagen man vil reservere lokalet på</param>
+        /// <returns>object af typen BookingData</returns>
         public BookingData GetSingleBookingByIdAndDay(int id, DateTime dag)
         {
             BookingData l = new BookingData();
